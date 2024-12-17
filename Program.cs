8d0e6dc8becbd2a26bd1cfdf3418e5fa48016171
TIB\orlov <orlov@tiburon-research.com>
2024-12-18 01:36:40 +0300

    Remove debug code

--- Program.cs
+++ Program.cs
@@ -1,23 +1,13 @@
 ﻿using System.Text.Json;
 
-var test = Directory.GetCurrentDirectory();
-
-var test2 = new DirectoryInfo(test);
-
-var test3 = test2.GetFiles();
-
-using var fileStream = File.OpenRead("BAD.mp4");
-//using var fileCopyStream = new MemoryStream();
-
-//fileStream.CopyTo(fileCopyStream);
-//fileCopyStream.Position = 0;
-
 var analyzerOptions = new FFMpegCore.FFOptions
 {
     LogLevel = FFMpegCore.Enums.FFMpegLogLevel.Verbose
 };
 
 var jsonSerializationOptions = new JsonSerializerOptions { WriteIndented = true };
+
+using var fileStream = File.OpenRead("BAD.mp4");
 
 var fileAnalysisResult = await FFMpegCore.FFProbe.AnalyseAsync(fileStream, analyzerOptions);
 Console.WriteLine($"Analysis STREAM:\n{JsonSerializer.Serialize(fileAnalysisResult, jsonSerializationOptions)}");
